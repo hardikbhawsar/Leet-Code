5b974f50853f540c6ff078755d4f226886193eaa
Hardik Bhawsar <hardik.13bhawsar@gmail.com>
2024-03-04 19:21:16 -0800

    Solution of Problem 189

---
+++ 189.Rotate array/mySolution.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+     reverse(nums.begin(), nums.end());
+     reverse(nums.begin(), nums.begin()+k);
+     reverse(nums.begin()+k, nums.end());   
+    }
+};
+
+int main(){
+Solution s;
+vector<int> nums; 
+
+nums.push_back(1);
+nums.push_back(2);
+nums.push_back(3);
+nums.push_back(4);
+nums.push_back(5);
+nums.push_back(6);
+nums.push_back(7);
+int k =3;
+s.rotate(nums,k);
+for(int i=0; i< nums.size(); i++){
+    cout<<nums[i];
+}
+return 0;
+}
