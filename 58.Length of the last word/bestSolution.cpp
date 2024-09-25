76234275f82c808c5bb178e4e44fce58563f1481
Hardik Bhawsar <hardik.13bhawsar@gmail.com>
2024-09-25 13:42:38 -0700

    Implementation of best solution

--- 58.Length of the last word/bestSolution.cpp
+++ 58.Length of the last word/bestSolution.cpp
@@ -25,3 +25,26 @@ int ans = s.lengthOfLastWord(str);
 cout<< ans;
 return 0;
 }
+
+/*  This solution is good too! Just one for loop ~~~~~
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        bool counting = false;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                counting = true;
+                length++;
+            }
+            else if (counting) {
+                break;
+            }
+        }
+        
+        return length;
+    }
+};
+*/
