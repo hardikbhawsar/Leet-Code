8b899566fea2180211ea5a7ef5250b8242aa3b1b
Hardik Bhawsar <hardik.13bhawsar@gmail.com>
2024-09-26 22:28:12 -0700

    Implementation of problem 14

--- 14.Longest Common Prefix/mySolution.cpp
+++ 14.Longest Common Prefix/mySolution.cpp
@@ -1,3 +1,11 @@
+/*
+Time Complexity: O(nlogn)
+
+FAQ: 
+Time complexity of sort() funtion is nlogn. 
+It used a combination of quick sort, selection sort and heap sort. Optimized. 
+*/
+
 #include <iostream>
 #include<vector>
 #include<algorithm>
@@ -8,7 +16,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string ans = "";
         int end = strs.size();
-        sort(strs.begin(), strs.end());
+        sort(strs.begin(), strs.end());     // sort has nlogn time complexity
         string first = strs[0], last = strs[end-1];
         for(int i=0; i< min(first.size(),last.size()); i++){
             if(first[i] != last[i]){
@@ -26,7 +34,7 @@ vector<string> words;
 words.push_back("flow");
 words.push_back("flight");
 words.push_back("flower");
-words.push_back("");
+words.push_back("flag");
 string ans = s.longestCommonPrefix(words);
 cout<< ans;
 return 0;
